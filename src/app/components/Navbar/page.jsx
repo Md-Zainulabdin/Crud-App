26ef690759abf98ae6c5532ce55351a4e15cfa8a
Muhammad Zain-ul-Abdin Ansari <zainzabid4338@gmail.com>
2023-08-28 15:18:07 +0500

    create navbar

---
+++ src/app/components/Navbar/page.jsx
@@ -0,0 +1,17 @@
+import Link from "next/link";
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <nav className="w-full h-[70px] px-[30px] md:px-[50px] text-white bg-indigo-600 flex items-center justify-between">
+      <Link href={"/"} className="logo">
+        <h1 className="text-2xl font-semibold">CRUD APP</h1>
+      </Link>
+      <Link href={"/addTopic"} className="menu">
+        <span className="border-2 border-slate-300 transition duration-300 ease-in-out rounded-full px-5 py-2 hover:bg-white hover:text-[#222]">Add product</span>
+      </Link>
+    </nav>
+  );
+};
+
+export default Navbar;
